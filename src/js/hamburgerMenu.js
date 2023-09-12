5c4995fd488b7e2ffd03081832082e57908420d3
Alexandra1234 <alexandra.zakovinko>
2023-09-12 20:50:09 +0300

    hamburger menu

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -1,9 +1,16 @@
 const hamburger = document.querySelector(".hamburger-btn");
 const navMenu = document.querySelector(".hamburger-menu");
 const body = document.querySelector("body");
+const menuLinks = document.querySelectorAll(".hamburger-menu .nav__menu__item-link");
 
-hamburger.addEventListener("click", function () {
+hamburger.addEventListener("click", toggleMobileMenu);
+
+menuLinks.forEach(menuLink => {
+	menuLink.addEventListener("click", toggleMobileMenu)
+})
+
+function toggleMobileMenu() {
 	hamburger.classList.toggle("active");
-	hamburger.classList.contains('active') ? body.style.overflow = 'hidden' : body.style.overflow = 'auto';
 	navMenu.classList.toggle("active");
-});
+	hamburger.classList.contains("active") ? body.style.overflow = 'hidden' : body.style.overflow = 'auto';
+}
