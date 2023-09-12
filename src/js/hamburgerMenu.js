28d3c78221e78b6b89f40753125ec5591611c175
Alexandra1234 <alexandra.zakovinko>
2023-09-12 13:22:56 +0300

    add disabled scrolling page for mobile menu

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger-btn");
 const navMenu = document.querySelector(".hamburger-menu");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", function () {
 	hamburger.classList.toggle("active");
+	hamburger.classList.contains('active') ? body.style.overflow = 'hidden' : body.style.overflow = 'auto';
 	navMenu.classList.toggle("active");
 });
